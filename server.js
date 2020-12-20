246a8bb4dc936f5f0b8934d3d798e5511178f478
Noura AIT <noura.aitlhousse@gmail.com>
2020-12-20 18:52:19 +0100

    finall

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mangaList'));
+app.use(express.static('./dist/mangaList'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/mangaList/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/mangaList/'}),
+);
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
