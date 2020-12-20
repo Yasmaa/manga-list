95d65e516227226521940b5855bad3df2b62f7af
Noura AIT <noura.aitlhousse@gmail.com>
2020-12-20 19:34:26 +0100

    test2

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/mangaList'));
+app.use(express.static('./dist/manga-list'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/mangaList/'}),
+    res.sendFile('index.html', {root: 'dist/manga-list/'}),
 );
 
 app.listen(process.env.PORT || 8080);
